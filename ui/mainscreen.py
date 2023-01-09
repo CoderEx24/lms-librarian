aa327ff60ff06b6f8e79ca5168b27da49dc89b05
CoderEx24 <coderex24@protonmail.com>
2023-01-10 00:30:33 +0200

    implemented returning books

--- ui/mainscreen.py
+++ ui/mainscreen.py
@@ -1,4 +1,5 @@
 from kivy.uix.screenmanager import Screen
+from kivy.properties import NumericProperty
 from kivy.lang import Builder
 from kivy.app import App
 from os import path
@@ -8,6 +9,9 @@ import json
 Builder.load_file(path.join(path.dirname(__file__), 'mainscreen.kv'))
 
 class MainScreen(Screen):
+    
+    record_id = NumericProperty()
+
     def refresh(self):
         token = App.get_running_app().token
         headers = {'Authorization': f'Token {token}'}
@@ -17,6 +21,8 @@ class MainScreen(Screen):
         
         records_list = self.ids['borrow_records']
         records_list.data.clear()
+        records_list.refresh_from_data()
+
         for record in parsed_data:
             borrow_id = record['id']
             borrower = record['borrower']['user']['username']
@@ -27,5 +33,14 @@ class MainScreen(Screen):
 
             records_list.data.append({ 'text': f'{borrow_id} | {borrower} | {checker} | {book_title} | {date_of_borrowing} | {date_of_retrival}' })
             
+    def return_book(self):
+        token = App.get_running_app().token
+        headers = {'Authorization': f'Token {token}'}
+        data = {'record_id': int(self.record_id)}
+        
+        res = requests.post('http://127.0.0.1:8000/api/books/return/', data=data, headers=headers)
+
+        if res.status_code != 200:
+            self.ids['err_msg'].text = res.text
 
 
